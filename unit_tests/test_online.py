f3d84e2d30d687b9cf0d164aa1a332630bb4c239
mark <sn1572@gmail.com>
2022-04-23 16:22:18 -0900

    Making progress with the online unit test. Need to log it in to get the test video

--- unit_tests/test_online.py
+++ unit_tests/test_online.py
@@ -3,7 +3,7 @@ import requests
 import socket
 
 
-_test_file = 'shared/test.mp4'
+_test_file = 'shared/test.webm'
 
 
 def get_server_address():
@@ -11,11 +11,11 @@ def get_server_address():
     config.read('../uwsgi_configs/lan_config.ini')
     port = config['uwsgi']['http']
     ip_addr = socket.gethostname()
-    return f"{ip_addr}{port}/"
+    return f"http://{ip_addr}{port}/"
 
 
 def test_get_stream():
     url = get_server_address()
     url += _test_file
     response = requests.get(url)
-    print(response.headers)
+    return response
